15c13386b598898737474e5c30ef556b514b4eba
Adimac93 <Adimac93@gmail.com>
2021-04-06 17:21:34 +0200

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Geek
 {
@@ -24,7 +25,15 @@ namespace Geek
         {
             InitializeComponent();
 
+            
         }
+
+        private void UpedateTimer_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            lblTimeNow.Content = "TimeNow: " + Math.Round(dateTime.Subtract(score.time).TotalSeconds)+"s";
+        }
+
         Score score = new Score();
 
         private void Accept_Click(object sender, RoutedEventArgs e)
@@ -34,6 +43,7 @@ namespace Geek
                 DateTime dateTime = DateTime.Now;
                 TimeSpan resultTime = dateTime.Subtract(score.time);
                 double seconds = resultTime.TotalSeconds;
+                lblTime.Content = "Time: " + (int)seconds + "s";
                 score.points += 10;
                 lblScore.Content = "Score: " + score.points;
             }
@@ -42,9 +52,13 @@ namespace Geek
         private void Start_Click(object sender, RoutedEventArgs e)
         {
             DateTime dateTime = DateTime.Now;
-
-
             score.time = dateTime;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(UpedateTimer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+
+            
         }
     }
 }
